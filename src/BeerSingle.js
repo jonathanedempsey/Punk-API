16609f9f6fe4e62686fa33d916f5863622a50a5b
Jonathan Dempsey <jonathanedempsey@gmail.com>
2021-12-07 14:44:37 +0000

    Get the correct data for the beer single pages in console

--- src/BeerSingle.js
+++ src/BeerSingle.js
@@ -3,10 +3,32 @@ import { withRouter } from "react-router";
 import ErrorBoundary from "./ErrorBoundary";
 
 class BeerSingle extends Component {
+    state = { loading: true };
+
+    async componentDidMount() {
+        // console.log(`id = ${this.props.match.params.id}`);
+        const beerId = this.props.match.params.id - 1;
+        const res = await fetch(
+            `https://api.punkapi.com/v2/beers?id=${beerId}`
+        );
+        const json = await res.json();
+        this.setState(Object.assign({ loading: false }, json[beerId]));
+    }
+
     render () {
+        console.log(this.state);
+
         return (
-            <div>
-                <h1>Beer Single</h1>
+            <div className="beer__single">
+                <div className="wrapper">
+                    <div className="beer__single__image">
+
+                    </div>
+                    <div className="beer__single__content">
+                        <h1>lol</h1>
+                    </div>
+
+                </div>
             </div>
         )
     }
